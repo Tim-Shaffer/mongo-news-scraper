3a14bd6b1d16d39e4f5c0377358c7672b0c4c730
Tim-Shaffer <tcshaf24@ptd.net>
2020-04-22 20:45:48 -0400

    scraping is working and redirecting to list all the scraped articles

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -161,10 +161,25 @@ module.exports = function(app) {
       });
 
       // Send a message to the client
-      res.send("Scrape Complete");
+      // res.send("Scrape Complete");
+      res.redirect("/articles");
 
     });
 
+  });
+
+  // Route for getting all Articles from the db
+  app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
   });
 
 };
